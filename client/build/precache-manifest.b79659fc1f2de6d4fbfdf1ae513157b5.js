327bbf52b2c220a0a0fe017128772934c7fa8805
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-08-09 01:13:36 +0900

    yarn build

---
+++ client/build/precache-manifest.b79659fc1f2de6d4fbfdf1ae513157b5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95ad984117f613c2032768c0fde1fa48",
+    "url": "./index.html"
+  },
+  {
+    "revision": "737120d455007e91dafd",
+    "url": "./static/js/2.88692607.chunk.js"
+  },
+  {
+    "revision": "9ec16fb615380e94d913",
+    "url": "./static/js/main.b2be19cd.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
