722eb20177731a2daedc040475265eb1248024f8
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-08-11 23:41:39 +0900

    add controller

---
+++ src/controller/sfu.go
@@ -0,0 +1,44 @@
+package controller
+
+import (
+	"data-sfu/src/usecase"
+
+	"github.com/pion/webrtc/v2"
+)
+
+type JoinReq struct {
+	ROOM string `json:"room"`
+}
+
+type JoinRes struct {
+	SDP webrtc.SessionDescription `json:"sdp"`
+	UU  string                    `json:"uu"`
+}
+
+func Join(req JoinReq) (*JoinRes, error) {
+	sdp, uu, err := usecase.Join(req.ROOM)
+
+	if err != nil {
+		return nil, err
+	}
+
+	res := &JoinRes{SDP: sdp, UU: uu}
+	return res, nil
+}
+
+type AnswerReq struct {
+	ROOM string `json:"room"`
+	UU   string `json:"uu"`
+	TYPE string `json:"type"`
+	SDP  string `json:"sdp"`
+}
+
+func Answer(req AnswerReq) error {
+	err := usecase.Answer(req.ROOM, req.UU, req.TYPE, req.SDP)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
